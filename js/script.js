12e4a4c3658d7e40551bf7ac15600ca080cdc1f8
Jaroslaw Kubiak <kubiakjarek@outlook.com>
2020-05-14 12:09:11 +0100

    rework css and add listening at js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
+    // button sign up activation
 const check = document.querySelector('.signup__checkbox');
-const cover =document.querySelector('.signup__submit--cover');
+const cover =document.querySelector('.signup__submit');
 
 function stow(){
     cover.classList.toggle('active');
 }
-
 check.addEventListener('click', stow);
+
+    // variables
+const formSignUp = document.querySelector('#signup');
+const formLogin = document.querySelector('#login');
+
+
+    // listening
+formSignUp.addEventListener('submit', validateSignuUp);
+formLogin.addEventListener('submit', validateLogin);
+
+    // function
+function validateSignuUp() {
+    event.preventDefault();
+}
